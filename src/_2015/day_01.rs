25e35598fdd751faaa3330336ae3c3c30c983d9b
rajput-hemant <rajput.hemant2001@gmail.com>
2023-07-16 21:01:22 +0530

    solution for puzzle day 1, part 2 from 2015
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

--- src/_2015/day_01.rs
+++ src/_2015/day_01.rs
@@ -9,6 +9,29 @@ pub fn part_1(input: &str) -> String {
         .to_string()
 }
 
+pub fn part_2(input: &str) -> String {
+    let mut pos = 0;
+
+    input
+        .chars()
+        .enumerate()
+        .find_map(|(i, c)| {
+            pos += match c {
+                '(' => 1,
+                ')' => -1,
+                _ => unreachable!(),
+            };
+
+            if pos == -1 {
+                Some(i + 1)
+            } else {
+                None
+            }
+        })
+        .map(|x| x.to_string()) // we can map over the Option to convert it to a String
+        .unwrap()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -23,5 +46,11 @@ mod tests {
         assert_eq!(part_1("))("), "-1");
         assert_eq!(part_1(")))"), "-3");
         assert_eq!(part_1(")())())"), "-3");
+    }
+
+    #[test]
+    fn part_2_test() {
+        assert_eq!(part_2(")"), "1");
+        assert_eq!(part_2("()())"), "5");
     }
 }
