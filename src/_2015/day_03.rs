3e31e617d2b4408ce5dee94048d9e2067113feed
rajput-hemant <rajput.hemant2001@gmail.com>
2023-07-16 23:54:16 +0530

    solution for puzzle day 3, part 2 from 2015
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

--- src/_2015/day_03.rs
+++ src/_2015/day_03.rs
@@ -24,6 +24,33 @@ pub fn part_1(input: &str) -> String {
     .to_string()
 }
 
+pub fn part_2(input: &str) -> String {
+    let (mut x, mut y) = ([0, 0], [0, 0]); // coordinates for santa and robo-santa
+    let mut is_robo = 0; // flag to switch between santas
+
+    (input
+        .chars()
+        .fold(HashSet::new(), |mut acc, c| {
+            acc.insert((x[is_robo], y[is_robo])); // insert current santa's coordinates
+
+            match c {
+                '^' => y[is_robo] += 1,
+                'v' => y[is_robo] -= 1,
+                '>' => x[is_robo] += 1,
+                '<' => x[is_robo] -= 1,
+                _ => unreachable!(),
+            }
+
+            acc.insert((x[is_robo], y[is_robo]));
+
+            is_robo = 1 - is_robo; // switch santas
+
+            acc
+        })
+        .len())
+    .to_string()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -33,5 +60,12 @@ mod tests {
         assert_eq!(part_1(">"), "2");
         assert_eq!(part_1("^>v<"), "4");
         assert_eq!(part_1("^v^v^v^v^v"), "2");
+    }
+
+    #[test]
+    fn part_2_examples() {
+        assert_eq!(part_2("^v"), "3");
+        assert_eq!(part_2("^>v<"), "3");
+        assert_eq!(part_2("^v^v^v^v^v"), "11");
     }
 }
