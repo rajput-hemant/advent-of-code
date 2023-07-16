eed2054dec09206a7c7b0c35107fd4b4a1269a10
rajput-hemant <rajput.hemant2001@gmail.com>
2023-07-16 21:17:24 +0530

    solution for puzzle day 2, part 1 from 2015
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

---
+++ src/_2015/day_02.rs
@@ -0,0 +1,32 @@
+pub fn part_1(input: &str) -> String {
+    input
+        .lines()
+        .map(|line| {
+            let mut dims = line
+                .split('x')
+                .map(|x| x.parse::<i32>().unwrap())
+                .collect::<Vec<_>>();
+
+            dims.sort_unstable();
+
+            let l = dims[0];
+            let w = dims[1];
+            let h = dims[2];
+
+            ((2 * l * w) + (2 * w * h) + (2 * h * l)) // surface area
+             + (l * w) // the area of the smallest side
+        })
+        .sum::<i32>()
+        .to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_1_examples() {
+        assert_eq!(part_1("2x3x4"), "58");
+        assert_eq!(part_1("1x1x10"), "43");
+    }
+}
--- src/_2015/mod.rs
+++ src/_2015/mod.rs
@@ -1,1 +1,2 @@
 pub mod day_01;
+pub mod day_02;
