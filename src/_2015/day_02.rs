39bfb829ec5a58b0d24ec88e8d96ad05931eeeb4
rajput-hemant <rajput.hemant2001@gmail.com>
2023-07-16 21:20:21 +0530

    solution for puzzle day 2, part 2 from 2015
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

--- src/_2015/day_02.rs
+++ src/_2015/day_02.rs
@@ -19,6 +19,27 @@ pub fn part_1(input: &str) -> String {
         .sum::<i32>()
         .to_string()
 }
+pub fn part_2(input: &str) -> String {
+    input
+        .lines()
+        .map(|line| {
+            let mut dims = line
+                .split('x')
+                .map(|x| x.parse::<i32>().unwrap())
+                .collect::<Vec<_>>();
+
+            dims.sort_unstable();
+
+            let l = dims[0];
+            let w = dims[1];
+            let h = dims[2];
+
+            (2 * l) + (2 * w) // perimeter of the smallest side
+                + (l * w * h) // volume
+        })
+        .sum::<i32>()
+        .to_string()
+}
 
 #[cfg(test)]
 mod tests {
@@ -28,5 +49,11 @@ mod tests {
     fn part_1_examples() {
         assert_eq!(part_1("2x3x4"), "58");
         assert_eq!(part_1("1x1x10"), "43");
+    }
+
+    #[test]
+    fn part_2_examples() {
+        assert_eq!(part_2("2x3x4"), "34");
+        assert_eq!(part_2("1x1x10"), "14");
     }
 }
