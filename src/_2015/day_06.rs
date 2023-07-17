77003c2887b32e9ba3d5da6fdb281a72c95b5092
rajput-hemant <rajput.hemant2001@gmail.com>
2023-07-17 14:17:17 +0530

    solution for puzzle day 6, part 2 from 2015
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

--- src/_2015/day_06.rs
+++ src/_2015/day_06.rs
@@ -48,6 +48,58 @@ pub fn part_1(input: &str) -> String {
         .to_string()
 }
 
+pub fn part_2(input: &str) -> String {
+    input
+        .lines()
+        .map(|line| line.split(" ").collect::<Vec<_>>())
+        .fold(vec![vec![0; 1000]; 1000], |mut lights, line| {
+            match line[0] {
+                "turn" => match line[1] {
+                    "on" => {
+                        let (start_x, start_y) = parse_range(line[2]);
+                        let (end_x, end_y) = parse_range(line[4]);
+
+                        (start_x..=end_x).for_each(|i| {
+                            (start_y..=end_y).for_each(|j| {
+                                lights[i][j] += 1;
+                            });
+                        });
+                    }
+                    "off" => {
+                        let (start_x, start_y) = parse_range(line[2]);
+                        let (end_x, end_y) = parse_range(line[4]);
+
+                        (start_x..=end_x).for_each(|i| {
+                            (start_y..=end_y).for_each(|j| {
+                                if lights[i][j] > 0 {
+                                    lights[i][j] -= 1;
+                                }
+                            });
+                        });
+                    }
+                    _ => unreachable!(),
+                },
+                "toggle" => {
+                    let (start_x, start_y) = parse_range(line[1]);
+                    let (end_x, end_y) = parse_range(line[3]);
+
+                    (start_x..=end_x).for_each(|i| {
+                        (start_y..=end_y).for_each(|j| {
+                            lights[i][j] += 2;
+                        });
+                    });
+                }
+                _ => unreachable!(),
+            }
+
+            lights
+        })
+        .iter()
+        .map(|row| row.iter().sum::<usize>())
+        .sum::<usize>()
+        .to_string()
+}
+
 fn parse_range(range: &str) -> (usize, usize) {
     let range = range.split(",").collect::<Vec<_>>();
     (range[0].parse().unwrap(), range[1].parse().unwrap())
@@ -62,5 +114,11 @@ mod tests {
         assert_eq!(part_1("turn on 0,0 through 999,999"), "1000000");
         assert_eq!(part_1("toggle 0,0 through 999,0"), "1000");
         assert_eq!(part_1("turn off 499,499 through 500,500"), "0");
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(part_2("turn on 0,0 through 0,0"), "1");
+        assert_eq!(part_2("toggle 0,0 through 999,999"), "2000000");
     }
 }
