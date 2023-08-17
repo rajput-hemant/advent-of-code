4d50eca4c033781f699ce2029a6aabfb5625713e
rajput-hemant <rajput.hemant2001@gmail.com>
2023-08-17 21:54:28 +0530

    solve part 2 of puzzle from day 1, 2016
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

--- src/_2016/day_01.rs
+++ src/_2016/day_01.rs
@@ -18,7 +18,6 @@ pub fn part_1(input: &str) -> usize {
         .fold((0, 0, 0), |(x, y, dir), (turn, steps)| {
             // update the direction based on the turn ('R' for right, 'L' for left).
             // dir ranges from 0 to 3 (representing North, East, South, West respectively).
-            //
             let dir = (dir + if turn == 'R' { 1 } else { 3 }) % 4;
 
             // Update the position based on the direction and number of steps.
@@ -27,6 +26,26 @@ pub fn part_1(input: &str) -> usize {
 
     // calculate the Manhattan distance by taking the absolute sum of x and y coordinates
     (x.abs() + y.abs()) as usize
+}
+
+pub fn part_2(input: &str) -> usize {
+    let (mut x, mut y, mut dir) = (0, 0, 0);
+    let mut visited = std::collections::HashSet::new();
+
+    for (turn, steps) in parse_input(input) {
+        dir = (dir + if turn == 'R' { 1 } else { 3 }) % 4;
+
+        for _ in 0..steps {
+            x += COMPASS[dir].0;
+            y += COMPASS[dir].1;
+
+            if !visited.insert((x, y)) {
+                return (x.abs() + y.abs()) as usize;
+            }
+        }
+    }
+
+    unreachable!()
 }
 
 #[cfg(test)]
@@ -38,5 +57,10 @@ mod tests {
         assert_eq!(part_1("R2, L3"), 5);
         assert_eq!(part_1("R2, R2, R2"), 2);
         assert_eq!(part_1("R5, L5, R5, R3"), 12);
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(part_2("R8, R4, R4, R8"), 4);
     }
 }
