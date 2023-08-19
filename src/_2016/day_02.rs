294c8c61cbecc0cb55d5b244c0be7b266450c86a
rajput-hemant <rajput.hemant2001@gmail.com>
2023-08-19 10:25:53 +0530

    solve part 2 of puzzle from day 2, 2016
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

--- src/_2016/day_02.rs
+++ src/_2016/day_02.rs
@@ -28,6 +28,51 @@ pub fn part_1(input: &str) -> usize {
         .unwrap()
 }
 
+pub fn part_2(input: &str) -> String {
+    let keypad = vec![
+        vec![' ', ' ', '1', ' ', ' '],
+        vec![' ', '2', '3', '4', ' '],
+        vec!['5', '6', '7', '8', '9'],
+        vec![' ', 'A', 'B', 'C', ' '],
+        vec![' ', ' ', 'D', ' ', ' '],
+    ];
+
+    let (mut x, mut y) = (0, 2);
+
+    input
+        .lines()
+        .map(|line| {
+            for c in line.chars() {
+                match c {
+                    'U' => {
+                        if y > 0 && keypad[y - 1][x] != ' ' {
+                            y -= 1;
+                        }
+                    }
+                    'D' => {
+                        if y < 4 && keypad[y + 1][x] != ' ' {
+                            y += 1;
+                        }
+                    }
+                    'L' => {
+                        if x > 0 && keypad[y][x - 1] != ' ' {
+                            x -= 1;
+                        }
+                    }
+                    'R' => {
+                        if x < 4 && keypad[y][x + 1] != ' ' {
+                            x += 1;
+                        }
+                    }
+                    _ => unreachable!(),
+                }
+            }
+
+            keypad[y][x]
+        })
+        .collect()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -40,5 +85,10 @@ UUUUD"#;
     #[test]
     fn test_part_1() {
         assert_eq!(part_1(INPUT), 1985);
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(part_2("INPUT"), "5DB3");
     }
 }
