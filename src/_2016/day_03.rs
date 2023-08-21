f10ef7494ed46564ea369f73ce3bd21ce9568ebe
rajput-hemant <rajput.hemant2001@gmail.com>
2023-08-21 14:25:24 +0530

    solve part 2 of puzzle from day 3, 2016
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

--- src/_2016/day_03.rs
+++ src/_2016/day_03.rs
@@ -15,6 +15,31 @@ pub fn part_1(input: &str) -> usize {
         .count()
 }
 
+pub fn part_2(input: &str) -> usize {
+    input
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|x| x.parse::<usize>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>()
+        .chunks(3)
+        .fold(0, |mut count, lines| {
+            (0..3).for_each(|i| {
+                let mut dims = vec![lines[0][i], lines[1][i], lines[2][i]];
+
+                dims.sort();
+
+                if dims[0] + dims[1] > dims[2] {
+                    count += 1;
+                }
+            });
+
+            count
+        })
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -23,5 +48,20 @@ mod tests {
     fn test_part_1() {
         assert_eq!(part_1("5 10 25"), 0);
         assert_eq!(part_1("5 10 10"), 1);
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(
+            part_2(
+                "101 301 501
+102 302 502
+103 303 503
+201 401 601
+202 402 602
+203 403 603"
+            ),
+            6
+        );
     }
 }
