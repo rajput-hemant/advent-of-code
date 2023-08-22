62f865d4fac8a5b5eb5324458e57cca00adbc911
rajput-hemant <rajput.hemant2001@gmail.com>
2023-08-22 21:05:24 +0530

    solve part 1 of puzzle from day 4, 2016
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

---
+++ src/_2016/day_04.rs
@@ -0,0 +1,61 @@
+use lazy_regex::regex_captures;
+
+pub fn part_1(input: &str) -> usize {
+    input
+        .lines()
+        .map(|line| {
+            let (_, name, sector_id, checksum) =
+                regex_captures!(r"([a-z-]+)-(\d+)\[([a-z]+)\]", line).unwrap();
+
+            // remove dashes
+            let mut chars: Vec<_> = name.chars().filter(|&c| c != '-').collect();
+
+            // sort chars in alphabetical order
+            chars.sort_unstable();
+
+            // count consecutive chars
+            let mut counts: Vec<_> = chars.into_iter().fold(vec![], |mut counts, c| {
+                if let Some((prev, count)) = counts.last_mut() {
+                    if *prev == c {
+                        *count += 1;
+                    } else {
+                        counts.push((c, 1));
+                    }
+                } else {
+                    counts.push((c, 1));
+                }
+                counts
+            });
+
+            // sort by count, then by char
+            counts.sort_unstable_by(|a, b| b.1.cmp(&a.1).then(a.0.cmp(&b.0)));
+
+            // take the first 5 chars
+            let expected_checksum: String = counts.into_iter().take(5).map(|(c, _)| c).collect();
+            // compare checksums
+            if expected_checksum == checksum {
+                sector_id.parse::<usize>().unwrap()
+            } else {
+                0
+            }
+        })
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+        assert_eq!(
+            part_1(
+                "aaaaa-bbb-z-y-x-123[abxyz]\n\
+                a-b-c-d-e-f-g-h-987[abcde]\n\
+                not-a-real-room-404[oarel]\n\
+                totally-real-room-200[decoy]"
+            ),
+            151
+        );
+    }
+}
