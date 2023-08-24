586418247138eac2ad51e4ab240a17d07331e86b
rajput-hemant <rajput.hemant2001@gmail.com>
2023-08-24 15:19:50 +0530

    solve part 2 of puzzle from day 5, 2016
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

--- src/_2016/day_05.rs
+++ src/_2016/day_05.rs
@@ -7,6 +7,29 @@ pub fn part_1(input: &str) -> String {
         .collect()
 }
 
+pub fn part_2(input: &str) -> String {
+    let mut password = vec![' '; 8];
+    let mut count = 0;
+
+    for i in 0.. {
+        let hash = format!("{:x}", md5::compute(format!("{}{}", input, i)));
+        if hash.starts_with("00000") {
+            let pos = hash.chars().nth(5).unwrap();
+            if let Some(pos) = pos.to_digit(10).map(|pos| pos as usize) {
+                if pos < 8 && password[pos] == ' ' {
+                    password[pos] = hash.chars().nth(6).unwrap();
+                    count += 1;
+                    if count == 8 {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    password.into_iter().collect()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -14,5 +37,10 @@ mod tests {
     #[test]
     fn test_part_1() {
         assert_eq!(part_1("abc"), "18f47a30");
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(part_2("abc"), "05ace8e3");
     }
 }
