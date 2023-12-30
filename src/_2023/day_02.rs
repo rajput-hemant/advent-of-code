6a7333fcee6db843c6792fd60808f5b677fb22da
rajput-hemant <rajput.hemant2001@gmail.com>
2023-12-30 19:12:40 +0530

    feat: solved day 2 part 2 from 2023
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

--- src/_2023/day_02.rs
+++ src/_2023/day_02.rs
@@ -1,23 +1,10 @@
-pub fn part_01(input: &str) -> usize {
+pub fn part_1(input: &str) -> usize {
     input
         .lines()
         .map(|l| {
             let (game, sets) = l.split_once(":").unwrap();
 
-            let (blue_max, red_max, green_max) = sets
-                .split(|c| c == ',' || c == ';')
-                .map(|s| {
-                    let (qty, color) = s.trim().split_once(' ').unwrap();
-                    (qty.parse::<usize>().unwrap(), color)
-                })
-                .collect::<Vec<_>>()
-                .into_iter()
-                .fold((0, 0, 0), |(b, r, m), (qty, color)| match color {
-                    "blue" => (qty.max(b), r, m),
-                    "red" => (b, qty.max(r), m),
-                    "green" => (b, r, qty.max(m)),
-                    _ => unreachable!(),
-                });
+            let (blue_max, red_max, green_max) = parse_sets(sets);
 
             if red_max <= 12 && green_max <= 13 && blue_max <= 14 {
                 game.split_whitespace().nth(1).unwrap().parse().unwrap()
@@ -26,6 +13,36 @@ pub fn part_01(input: &str) -> usize {
             }
         })
         .sum()
+}
+
+pub fn part_2(input: &str) -> usize {
+    input
+        .lines()
+        .map(|l| {
+            let (blue_max, red_max, green_max) = parse_sets(l.split(":").nth(1).unwrap());
+
+            red_max * green_max * blue_max
+        })
+        .sum()
+}
+
+fn parse_sets(sets: &str) -> (usize, usize, usize) {
+    let (blue_max, red_max, green_max) = sets
+        .split(|c| c == ',' || c == ';')
+        .map(|s| s.trim().split_once(' ').unwrap())
+        .collect::<Vec<_>>()
+        .into_iter()
+        .fold((0, 0, 0), |(b, r, m), (qty, color)| {
+            let qty: usize = qty.parse().unwrap();
+            match color {
+                "blue" => (qty.max(b), r, m),
+                "red" => (b, qty.max(r), m),
+                "green" => (b, r, qty.max(m)),
+                _ => unreachable!(),
+            }
+        });
+
+    (blue_max, red_max, green_max)
 }
 
 #[cfg(test)]
@@ -40,6 +57,11 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
 
     #[test]
     fn test_part_1() {
-        assert_eq!(part_01(INPUT), 8);
+        assert_eq!(part_1(INPUT), 8);
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(part_2(INPUT), 2286);
     }
 }
